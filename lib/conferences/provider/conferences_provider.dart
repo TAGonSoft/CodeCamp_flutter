8aa67ae17b0f57e30c400fd4a1f2235a76381378
Alex Terente <alex.terente@gmail.com>
2020-07-24 13:30:11 +0300

    addrequest to fetch conferences

--- lib/conferences/provider/conferences_provider.dart
+++ lib/conferences/provider/conferences_provider.dart
@@ -4,17 +4,37 @@ import 'package:CodeCamp/conferences/model/conference.dart';
 import 'package:CodeCamp/helpers/FilesHelper.dart';
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+
+class HTTPHelper {
+  static const baseURL = 'https://connect.codecamp.ro/api/Conferences';
+}
+
 class ConferencesProvider with ChangeNotifier {
   List<Conference> _conferences = [];
 
   ConferencesProvider() {
-    fetchData();
+    loadLocalData();
+    fetchRemoteData();
   }
 
-  Future<void> fetchData() async {
+  Future<void> fetchRemoteData() async {
+    try {
+      final response = await http.get(HTTPHelper.baseURL);
+      print('Response:${json.decode(response.body)}');
+      FilesHelper.saveConferencesJson(response.body);
+      loadLocalData();
+    } catch (error) {
+      print('Error:$error');
+    }
+  }
+
+  Future<void> loadLocalData() async {
     final conferencesString = await FilesHelper.getConferencesJson();
     final jsonResult = json.decode(conferencesString);
-    final conferences = jsonResult.map<Conference>((json) => Conference.fromJson(json)).toList();
+    final conferences = jsonResult
+        .map<Conference>((json) => Conference.fromJson(json))
+        .toList();
     print('Conferecnes:$conferences');
     _conferences = conferences;
     notifyListeners();
--- lib/helpers/FilesHelper.dart
+++ lib/helpers/FilesHelper.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/services.dart' show rootBundle;
-
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
 
@@ -16,52 +14,23 @@ class FilesHelper {
     return '$assetsFolder/$jsonFilesFolder/$fileName.$jsonFileExtension';
   }
 
-  static Future<String> getConferencesJson() async {
+  static Future<String> getConferencesFilePath() async {
     final documentsPath = await syspaths.getApplicationDocumentsDirectory();
-    final conferencesPath =
-        path.join(documentsPath.path, '$conferencesFileName.$jsonFileExtension');
+    return path.join(documentsPath.path, '$conferencesFileName.$jsonFileExtension');
+  }
 
-    File conferencesFile;
+  static Future<void> saveConferencesJson(String conferencesJson) async {
+    final conferencesPath = await getConferencesFilePath();
+    final conferencesFile = File(conferencesPath);
+    await conferencesFile.writeAsString(conferencesJson);
+  }
 
-    if (FileSystemEntity.typeSync(conferencesPath) ==
-        FileSystemEntityType.notFound) {
-      print('Conferences file not found try to copy from bundle.');
-      final conferencesData = await rootBundle.loadString(FilesHelper.getBundlePath(conferencesFileName));
-      conferencesFile = File(conferencesPath);
-      try {
-        print(documentsPath.path);
-        await conferencesFile.writeAsString(conferencesData);
-      } catch (error) {
-        print(error);
-      }
-    }
+  static Future<String> getConferencesJson() async {
+    final conferencesPath = await getConferencesFilePath();
 
-    conferencesFile = File(conferencesPath);
+    final conferencesFile = File(conferencesPath);
     String conferences = await conferencesFile.readAsString();
     print(conferences);
     return conferences;
   }
-
-//  Future<Database> initializeDatabase() async {
-//
-//    Directory directory = await getApplicationDocumentsDirectory();
-//    var dbPath = join(directory.path, "ch_departures.db");
-//
-//    // copy db file from Assets folder to Documents folder (only if not already there...)
-//    if (FileSystemEntity.typeSync(dbPath) == FileSystemEntityType.notFound) {
-//      ByteData data = await rootBundle.load("assets/ch_departures.db");
-//      writeToFile(data, dbPath);
-//    }
-//
-//    // Open/create the database at a given path
-//    var departuresDatabase = await openDatabase(dbPath);
-//    return departuresDatabase;
-//  }
-//
-//  // HERE IS WHERE THE CODE CRASHES (WHEN TRYING TO WRITE THE LOADED BYTES)
-//  void writeToFile(ByteData data, String path) {
-//    final buffer = data.buffer;
-//    return new File(path).writeAsBytesSync(
-//        buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
-//  }
 }
